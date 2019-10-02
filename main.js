abf429ef252cfbba2dca45ba8829e64087de0f64
Dennis Sabinin <sawyer7071@gmail.com>
2019-10-02 17:23:55 +0300

    added JS tabs

---
+++ main.js
@@ -0,0 +1,20 @@
+const tabItem = document.querySelectorAll('.tab-item');
+const tabContentItem = document.querySelectorAll('.tab-content-item');
+
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    this.classList.add('tab-border');
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add('show');
+};
+
+function removeBorder() {
+    tabItem.forEach(item => item.classList.remove('tab-border'));
+};
+
+function removeShow() {
+    tabContentItem.forEach(item => item.classList.remove('show'));
+}
+
+tabItem.forEach(item => item.addEventListener('click', selectItem));
